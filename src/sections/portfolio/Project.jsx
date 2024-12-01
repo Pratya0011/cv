aa0ebfac41391e522cd7dd64792bbf527cfff190
Pratya0011 <pratya0011@gmail.com>
2024-12-01 23:11:54 +0530

    updates

--- src/sections/portfolio/Project.jsx
+++ src/sections/portfolio/Project.jsx
@@ -1,20 +1,45 @@
-import Card from '../../components/Card'
+import Card from "../../components/Card";
 
-
-const Project = ({project}) => {
+const Project = ({ project }) => {
   return (
     <Card className="portfolio__project">
-        <div className="portfolio__project-image">
-            <img src={project.image} alt="Portfolio Project Image" />
-        </div>
-        <h4>{project.title}</h4>
+      <div className="portfolio__project-image">
+        <img src={project.image} alt="Portfolio Project Image" />
+      </div>
+      <h4>{project.title}</h4>
+      {project?.category === "Techademy" ? (
+        <ul>
+          {project.desc.split("\n").map((item, index) => (
+            <li style={{ padding: 2 }} key={index}>
+              {item.replace("-", "").trim()}
+            </li>
+          ))}
+        </ul>
+      ) : (
         <p>{project.desc}</p>
+      )}
+      {project?.category !== "Techademy" && (
         <div className="portfolio__project-cta">
-            <a href={project.demo} className="btn sm" target="_blank" rel="noopener noreferrer">Demo</a>
-            <a href={project.github} className="btn sm primary" target="_blank" rel="noopner noreferrer">Gitbub</a>
+          <a
+            href={project.demo}
+            className="btn sm"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Demo
+          </a>
+          <a
+            href={project.github}
+            className="btn sm primary"
+            target="_blank"
+            rel="noopner noreferrer"
+          >
+            Gitbub
+          </a>
         </div>
+      )}
     </Card>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -3,10 +3,36 @@ import Image2 from "../../assets/project2.png";
 import Image3 from "../../assets/project3.png";
 import Image4 from "../../assets/project4.png";
 import Image5 from "../../assets/project5.png";
+import b2a from "../../assets/techademyCampus.jpg";
+import yaksha from "../../assets/yaksha.jpeg";
 
 const data = [
   {
     id: 1,
+    category: "Techademy",
+    image: yaksha,
+    title: "Yaksha Assessment Platform",
+    desc: `- Developed  coding assessments, integrating an in-browser IDE with non-editable code sections,Enabled functionality for test case execution and dynamic coding language changes.
+  - Designed the user flow for viewing upcoming, live, qualified, and not qualified assessments, along with results displayed in an analytical format.
+  - Implemented a feature to download results for offline use.
+  - Built a product catalogue feature allowing tenants to view, request, and add skills and categories to their catalog for personalized training content.
+  - Implemented a timer to enforce a minimum submission duration during the test-taking flow.
+  - Developed a leaderboard integrated into user profiles, displaying ranks and performance in hackathons.
+  - Created a user analytics dashboard for admins to view assessment data for enrolled users, enabling better monitoring and insights.`,
+  },
+  {
+    id: 2,
+    category: "Techademy",
+    image: b2a,
+    title: "Techademy Campus (B2A)",
+    desc: `- Integrated Keycloak Identity Provider for secure authentication and role-based access control.
+- Developed a user management system to onboard and manage users with different roles (students, instructors, and admins).
+- Implemented program management features for annual patterns, semester-based courses, and short-term programs.
+- Built dynamic role-specific dashboards to provide actionable insights and improve user productivity.
+- Designed an Assessment Repository feature, enabling instructors to create, assign, and manage assessments while allowing students to take them seamlessly.`,
+  },
+  {
+    id: 3,
     category: "Mern",
     image: Image1,
     title: "Flixxit (Mern)",
@@ -15,7 +41,7 @@ const data = [
     github: "https://github.com/Pratya0011/Flixxit",
   },
   {
-    id: 2,
+    id: 4,
     category: "React",
     image: Image2,
     title: "Pizza Mania (Frontend)",
@@ -28,7 +54,7 @@ const data = [
     github: "https://github.com/Pratya0011/pizzamania",
   },
   {
-    id: 3,
+    id: 5,
     category: "Javascript",
     image: Image3,
     title: "Arenamart E-commerce",
@@ -37,7 +63,7 @@ const data = [
     github: "https://github.com/Pratya0011/ArenaMart-Ecommerce",
   },
   {
-    id: 4,
+    id: 6,
     category: "Javascript",
     image: Image4,
     title: "Movie Mania",
@@ -46,7 +72,7 @@ const data = [
     github: "https://github.com/Pratya0011/mini-project-builder-movie-mania",
   },
   {
-    id: 5,
+    id: 7,
     category: "Javascript",
     image: Image5,
     title: "Smart Watch",
